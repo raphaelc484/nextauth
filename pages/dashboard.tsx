ef3d40369edebc514020e8ba33ad7b7f8b53aba8
raphaelc484 <raphaelc484@hotmail.com>
2021-06-30 17:41:46 -0300

    ok

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,22 +1,27 @@
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContexts";
+import { useCan } from "../hooks/useCan";
 import { setupApiClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut, isAuthenticated } = useContext(AuthContext);
 
   useEffect(() => {
-    api
-      .get("/me")
-      .then((response) => console.log(response))
-      .catch((err) => console.log(err));
+    api.get("/me").then((response) => console.log(response));
   }, []);
 
   return (
     <>
       <h1>Dash:{user?.email}</h1>
+
+      <button onClick={signOut}>Sign Out</button>
+
+      <Can permissions={["metrics.list"]}>
+        <div>Métricas</div>
+      </Can>
     </>
   );
 }
--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -5,14 +5,25 @@ import {
 } from "next";
 import { destroyCookie, parseCookies } from "nookies";
 import { AuthTokenError } from "../services/errors/AuthTokenError";
+import decode from "jwt-decode";
+import { validateUserPermissions } from "./validateUserPermissions";
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function withSSRAuth<P>(
+  fn: GetServerSideProps<P>,
+  options?: WithSSRAuthOptions
+): GetServerSideProps {
   return async (
     ctx: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
+    const token = cookies["nextauth.token"];
 
-    if (!cookies["nextauth.token"]) {
+    if (!token) {
       return {
         redirect: {
           destination: "/",
@@ -20,6 +31,30 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
         },
       };
     }
+
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[] }>(token);
+
+      const { permissions, roles } = options;
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if (!userHasValidPermissions) {
+        return {
+          // Caso vc n possua uma pagina onde todos tenham acesso
+          // notFound: true,
+          redirect: {
+            destination: "/dashboard",
+            permanent: false,
+          },
+        };
+      }
+    }
+
     try {
       return await fn(ctx);
     } catch (err) {
